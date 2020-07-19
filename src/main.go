f1c74c787b01c390b3bd3fe85bf836a99974215d
Jun Seok Lee <lego3410@gmail.com>
2020-07-20 04:03:36 +0900

    Just print simple reason of choosing Go

---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go is chosen as the programming language of this project, featuring fast and concise development, performant parallelism, and wide user base.");
+}
